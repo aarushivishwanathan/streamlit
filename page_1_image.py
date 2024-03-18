8a0a483197b9a15c0d6608f704c897892baed7a4
aarushivishwanathan <87535205+aarushivishwanathan@users.noreply.github.com>
2024-03-18 16:04:19 +0530

    Add files via upload

---
+++ page_1_image.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+def predict_image(image):
+    return(1)
+      
+
+#with st.sidebar:
+#    st.page_link("app.py", label="Home", icon="🏠")
+#    st.page_link("pages/page_1_image.py", label="Image Deepfake Detection", icon="📷")
+#    st.page_link("pages/page_2_video.py", label="Video Deepfake Detection", icon="🎥")
+#    st.page_link("pages/page_3_audio.py", label="Audio Deepfake Detection", icon="🔉")
+#    st.page_link("http://www.google.com", label="Report", icon="📄")
+
+#st.title('Image Deepfake Detection')
+xy = st.file_uploader("Upload a video file", type=["png", "jpg"])
+
+if xy is not None:
+    prediction=predict_image(xy)
+
+    if prediction==1:
+        st.write("The image is a deepfake!")
+    else:
+        st.write("The image is not a deepfake.")
+else: 
+    print("Error Processing image.")
