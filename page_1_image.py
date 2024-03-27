b48556cbd260bd1015b8c4e646d859ca720f8af0
aarushivishwanathan <87535205+aarushivishwanathan@users.noreply.github.com>
2024-03-27 15:17:37 +0530

    Update page_1_image.py

--- page_1_image.py
+++ page_1_image.py
@@ -20,14 +20,14 @@ if file is not None:
     #file_extension = get_file_extension(file)
     
     #if file_extension in ['.jpg', '.png', '.jpeg']:
-    st.write("Processing Audio")
+    st.write("Processing Image")
     
     prediction = predict_image(file)
     
     if prediction == 1:
-        st.error("The audio is a deepfake!",icon='🚨')
+        st.error("The image is a deepfake!",icon='🚨')
     elif prediction == 0:
-        st.success("The audio is not a deepfake!",icon='✅')
+        st.success("The image is not a deepfake!",icon='✅')
     else:
         st.warning('Error: Face not detected')
         
